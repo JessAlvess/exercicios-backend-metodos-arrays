676a7f5c6dfdfbaa93333e156a68cc9ab812d652
Jessé Alves <jessealvespr@outlook.com>
2024-02-26 20:25:34 -0300

    ex5

--- 05/scripts.js
+++ 05/scripts.js
@@ -1,1 +1,23 @@
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+
+function agendarPaciente(arr, paciente) {
+    arr.push(paciente)
+    return arr.join(', ')
+}
+
+console.log(agendarPaciente(pacientes, 'Jessé'));
+
+function atenderPaciente(arr, paciente) {
+    arr.shift()
+    return arr.join(', ')
+}
+
+console.log(atenderPaciente(pacientes));
+
+
+function cancelarAtendimento(arr, paciente) {
+    arr.splice(arr.indexOf(paciente), 1)
+    return arr.join(', ')
+}
+
+console.log(cancelarAtendimento(pacientes, 'Maria'));
