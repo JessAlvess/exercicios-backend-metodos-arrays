9f4d12f1a3e83d93fdef4fee662aa4dd0e9dfa43
Jessé Alves <jessealvespr@outlook.com>
2024-02-26 20:02:09 -0300

    ex3

---
+++ 03/scripts.js
@@ -0,0 +1,10 @@
+const nomes = ['Ford Ká', 'Ranger', 'Hilux', 'Corola', 'Fusca', 'Chevete', 'Brasilia'];
+const posicao = 3;
+
+function print(arr, position) {
+    
+
+    return console.log(`${arr.slice(position, position+3).join(' - ')}`)
+}
+
+print(nomes, posicao)
