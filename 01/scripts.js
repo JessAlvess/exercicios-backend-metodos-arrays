a493c9d171476df9a906792a047af6df474317c5
Jessé Alves <jessealvespr@outlook.com>
2024-02-26 19:22:56 -0300

    initial commit

---
+++ 01/scripts.js
@@ -0,0 +1,8 @@
+const frutas = ['Banana', 'Maçã', 'Abacaxi', 'Pêra', 'Uva'];
+
+console.log(frutas.reverse().join(', '))
+
+frutas.reverse().pop()
+frutas.shift()
+frutas.push('Melão')
+console.log(frutas);
