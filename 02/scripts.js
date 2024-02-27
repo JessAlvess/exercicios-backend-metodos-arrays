ca2807c069e4d697ec9e9bb3c6fb26cd34c0d86e
Jessé Alves <jessealvespr@outlook.com>
2024-02-27 19:03:04 -0300

    ex2

--- 02/scripts.js
+++ 02/scripts.js
@@ -1,16 +1,18 @@
 const nomes = ['Juninho', 'Vidal', 'Guido', 'Dani', 'Ruli', 'Diego'];
-const tamanhoDoGrupo = 4;
+const tamanhoDoGrupo = 3;
 
 const grupos = (arr, tamanho) => {
-    let grupoCompleto = Math.floor(arr.length / 4)
+    let grupoCompleto = Math.floor(arr.length / tamanhoDoGrupo)
     let indexStart = 0
 
     for (let i = 0; i <= grupoCompleto; i++) {
         if (i !== grupoCompleto) { 
-            console.log(`Grupo ${i+1}: ${arr.slice(indexStart,indexStart+4).join(', ')}.`)
-            indexStart += 4
+            console.log(`Grupo ${i+1}: ${arr.slice(indexStart,indexStart+tamanhoDoGrupo).join(', ')}.`)
+            indexStart += tamanhoDoGrupo
         } else {
-            console.log(`Grupo ${i+1}: ${arr.slice(indexStart).join(', ')}.`)
+            if (indexStart < arr.length) {
+                console.log(`Grupo ${i+1}: ${arr.slice(indexStart).join(', ')}.`)
+            }
         }
     }
 }
